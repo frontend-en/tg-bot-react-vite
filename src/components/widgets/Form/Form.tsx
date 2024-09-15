d70f7c4dd00d732238a9c05ad73c5f98ea7a9f05
k.revyakina <k.revyakina@kata.academy>
2024-09-15 16:54:42 +0300

    add form

--- src/components/widgets/Form/Form.tsx
+++ src/components/widgets/Form/Form.tsx
@@ -1,15 +1,44 @@
 import classNames from "classnames";
-// import Button from "../../ui/Button/Button";
-// import { useTelegram } from "../../hooks/useTelegram";
+import { useTelegram } from "../../hooks/useTelegram";
 
 import './Form.css'
+import { useEffect, useState } from "react";
 
 const Form = () => {
-    // const { onClose, user } = useTelegram()
+    const { tg } = useTelegram()
+
+    const [formData, setFormData] = useState({
+        country: '',
+        city: '',
+        subject: 'physical'
+    })
+    useEffect(()=> {
+        tg.MainButton.setParams({
+            text: 'Отправить данные'
+        })
+    }, [])
+
+    useEffect(()=>{
+        if(!formData.city || !formData.country) {
+            tg.MainButton.hide()
+        } else {
+            tg.MainButton.show()
+        }
+    }, [formData.city, formData.country])
+
+    const handleChangeFormData = (event: any) => {
+        setFormData({ ...formData, [event.target.name]: event.target.value });
+    }
 
     return (
-        <form action="#" className={classNames('form')}>
-            Form
+        <form action="#" className={classNames("form")}>
+            <h3>Введите ваши данные</h3>
+            <input type="text" name="country" onChange={(e) => handleChangeFormData(e)} value={formData.country} className={classNames("input")} placeholder="Страна" />
+            <input type="text" name="city" onChange={(e) => handleChangeFormData(e)} value={formData.city} className={classNames("input")} placeholder="Город" />
+            <select value={formData.subject} onChange={(e) => handleChangeFormData(e)} name="subject" id="" className={classNames("select")}>
+                <option value="physical">Физ. лицо</option>
+                <option value="legal">Юр. лицо</option>
+            </select>
         </form>
     )
 }
